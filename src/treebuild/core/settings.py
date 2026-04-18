1d8b5b86d346e8394f872351bb7dc75d94dcac41
KleinMisha <klein.misha@gmail.com>
2026-04-18 17:50:42 +0200

    Define priority of loading variables for pydantic-settings model. Followed tutorial shown in official documentation.

--- src/treebuild/core/settings.py
+++ src/treebuild/core/settings.py
@@ -2,9 +2,14 @@
 
 from pathlib import Path
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    TomlConfigSettingsSource,
+)
 
-from treebuild.rendering.renderer_types import RendererType
+from treebuild.rendering.factory import RenderMethod
 
 # DEFAULTS TO FALL BACK TO:
 DEFAULT_SESSION_FILE = Path().home() / ".config" / "treebuild" / "session_tree.txt"
@@ -13,12 +18,38 @@ DEFAULT_SESSION_FILE = Path().home() / ".config" / "treebuild" / "session_tree.t
 class TreeBuildSettings(BaseSettings):
     """Default settings"""
 
-    session_file: Path = DEFAULT_SESSION_FILE
-    renderer: RendererType = RendererType.PLAIN
-
     model_config = SettingsConfigDict(
         toml_file=Path().home() / ".config" / "treebuild" / "config.toml"
     )
+    session_file: Path = DEFAULT_SESSION_FILE
+    renderer: RenderMethod = RenderMethod.PLAIN
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        """
+        Priority of loading variables
+
+        1. Constructor (TreeBuildSettings(key=value), aka the __init__() method)
+        2. Environment variables (so you can 'env export variables' in terminal or `.zshrc`-like file)
+        3. config TOML file (~/.config/treebuild/config.toml)
+
+        ---
+        NOTE: Setting environment variables is optional, hence excluding some manual overwrite during instantiation,
+        values will be parsed from the TOML file.
+        """
+
+        return (
+            init_settings,
+            env_settings,
+            TomlConfigSettingsSource(settings_cls),
+        )
 
 
 def get_settings() -> TreeBuildSettings:
