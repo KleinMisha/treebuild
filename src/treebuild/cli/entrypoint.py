aaa08a02908a512d04a6015547a669d268aec431
KleinMisha <klein.misha@gmail.com>
2026-04-30 00:54:34 +0200

    Use logger to show messages + included verbosity / quiet flags to increase / suppress messages shown to user.

--- src/treebuild/cli/entrypoint.py
+++ src/treebuild/cli/entrypoint.py
@@ -1,14 +1,32 @@
 """CLI entrypoint & routing."""
 
-from typer import Typer
+from typing import Annotated
+
+from typer import Option, Typer
 
 from treebuild.cli.harvest import harvest_app
 from treebuild.cli.treebuild import app as main_app
+from treebuild.core.logging import setup_logging
 
 # Tool will call this `app()` instance
 app = Typer()
 app.add_typer(main_app)
 app.add_typer(harvest_app, name="harvest")
+
+
+@app.callback()
+def main(
+    verbose: Annotated[
+        bool, Option("--verbose", "-v", help="Show detailed output.")
+    ] = False,
+    quiet: Annotated[
+        bool, Option("--quiet", "-q", help="Suppress all messages except errors.")
+    ] = False,
+) -> None:
+
+    # setup logging with logging level based on flags
+    setup_logging(verbose=verbose, quiet=quiet)
+
 
 if __name__ == "__main__":
     app()
