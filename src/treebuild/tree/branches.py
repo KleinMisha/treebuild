44ff349d8af34d4a1df15fd20b44eeac292a5729
KleinMisha <klein.misha@gmail.com>
2026-05-01 02:20:38 +0200

    Get all the paths (as str) of a given tree. Only shows deepest path. Usefull for logging.

--- src/treebuild/tree/branches.py
+++ src/treebuild/tree/branches.py
@@ -5,6 +5,7 @@ A Directory tree consists of:
 """
 
 from dataclasses import dataclass, field
+from pathlib import Path
 
 # A filename represents a Leaf in the tree.
 type Leaf = str
@@ -30,6 +31,16 @@ class Branch:
     def add_child_branch(self, branch: "Branch") -> None:
         self.branches.append(branch)
 
+    def all_paths(self, current_path: Path) -> list[Path]:
+        """All the paths in this (child) directory."""
+        paths = [current_path]
+        paths.extend([current_path / leaf for leaf in self.leaves])
+
+        # traversal / recursion:
+        for branch in self.branches:
+            paths.extend(branch.all_paths(current_path / branch.name))
+        return paths
+
 
 @dataclass
 class Tree:
@@ -49,3 +60,21 @@ class Tree:
     def add_branch(self, branch: Branch) -> None:
         """Wrapper for convenience"""
         self.root.add_child_branch(branch)
+
+    @property
+    def paths(self) -> list[str]:
+        """Paths (at deepest level) as strings"""
+        all_paths = [Path(self.root.name)]
+        all_paths.extend(self.root.all_paths(Path(self.root.name)))
+
+        # Only keep root/dir/some.file , and remove root and root/dir in this case
+        deepest_paths = [
+            str(path)
+            for path in all_paths
+            if not any(
+                other_path != path and path in other_path.parents
+                for other_path in all_paths
+            )
+        ]
+
+        return deepest_paths
--- tests/treebuild/tree/test_branches.py
+++ tests/treebuild/tree/test_branches.py
@@ -1,8 +1,11 @@
 """Unit tests for src/treebuilder/tree/branches.py"""
 
+from pathlib import Path
+
 from treebuild.tree.branches import Branch, Tree
 
 
+# --- constructing a Tree / Branches ---
 def test_branch_initializes_with_empty_leaves_and_branches() -> None:
     branch = Branch("new-branch")
     assert branch.branches == []
@@ -50,3 +53,169 @@ def test_add_branch_to_tree_calls_root_branch() -> None:
     tree = Tree(directory)
     tree.add_leaf(filename="some_file.txt")
     assert tree.root.leaves == ["some_file.txt"]
+
+
+# --- Getting all paths ---
+def test_paths_empty_tree() -> None:
+    """If tree is empty, should only return ["root/"]"""
+    # build tree
+    tree = Tree(Branch("root"))
+
+    # get paths
+    expected_paths = ["root"]
+    assert set(tree.paths) == set(expected_paths)
+
+
+def test_paths_only_files_in_root() -> None:
+    """A directory with no subdirectories, just files"""
+    # Construct set of expected paths
+
+    # build tree
+    tree = Tree(Branch("root"))
+    filenames = sorted(["file_1.txt", "code.py", "more_code.java", "dataset.xlsx"])
+    expected_items = [tree.root.name / Path(fn) for fn in filenames]
+    for f in filenames:
+        tree.add_leaf(f)
+
+    # get paths
+    expected_paths = [str(p) for p in expected_items]
+    assert set(tree.paths) == set(expected_paths)
+
+
+def test_paths_single_empty_subdir_under_root() -> None:
+    """
+    Directory structure: ./subdir
+    """
+    # build tree
+    tree = Tree(Branch("root"))
+    tree.add_branch(Branch("folder"))
+
+    # get paths
+    expected_paths = ["root/folder"]
+    assert set(tree.paths) == set(expected_paths)
+
+
+def test_paths_single_subdir_under_root() -> None:
+    """
+    Directory structure: ./subdir/file.txt , given there are a couple of files in this sub-directory
+    """
+    # build tree
+    filenames = sorted(["file_1.txt", "code.py", "more_code.java", "dataset.xlsx"])
+    tree = Tree(Branch("root"))
+    folder = Branch("folder")
+    for f in filenames:
+        folder.add_leaf(f)
+    tree.add_branch(folder)
+
+    # get paths
+    expected_items = [Path(tree.root.name) / "folder" / fn for fn in filenames]
+    expected_paths = [str(p) for p in expected_items]
+    assert set(tree.paths) == set(expected_paths)
+
+
+def test_paths_multiple_subdirs_at_same_level() -> None:
+    """
+    Directory structure: ./subdir/file.txt, and ./second_folder/file.txt.
+    """
+    # build tree
+    files_in_first_folder = sorted(
+        ["first_file.py", "second_file.css", "third_file.md"]
+    )
+    files_in_second_folder = sorted(["another_file.txt"])
+
+    first_folder = Branch("folder1")
+    for f in files_in_first_folder:
+        first_folder.add_leaf(f)
+
+    second_folder = Branch("folder2")
+    for f in files_in_second_folder:
+        second_folder.add_leaf(f)
+
+    tree = Tree(Branch("root"))
+    tree.add_branch(first_folder)
+    tree.add_branch(second_folder)
+
+    # get paths
+    expected_items = [
+        Path(tree.root.name) / "folder1" / fn for fn in files_in_first_folder
+    ]
+    expected_items.extend(
+        [Path(tree.root.name) / "folder2" / fn for fn in files_in_second_folder]
+    )
+    expected_paths = [str(p) for p in expected_items]
+    assert set(tree.paths) == set(expected_paths)
+
+
+def test_paths_nested_subdirs() -> None:
+    """
+    Directory structure: ./subdir/subsubdir/file.txt (a file in a nested directory)
+    """
+    # build tree
+    files_in_second_folder = sorted(
+        ["first_file.py", "second_file.css", "third_file.md"]
+    )
+    first_folder = Branch("folder1")
+    second_folder = Branch("folder2")
+    for f in files_in_second_folder:
+        second_folder.add_leaf(f)
+    first_folder.add_child_branch(second_folder)
+    tree = Tree(Branch("root"))
+    tree.add_branch(first_folder)
+
+    # get paths
+    expected_items = [
+        Path(tree.root.name) / "folder1" / "folder2" / fn
+        for fn in files_in_second_folder
+    ]
+    expected_paths = [str(p) for p in expected_items]
+    assert set(tree.paths) == set(expected_paths)
+
+
+def test_paths_mixed_leaves_and_branches() -> None:
+    """
+    Directory structure: ./file_in_root.txt, ./subdir/file.py, ./subdir/subsubdir/more_files.py
+    Include a third folder that is empty.
+    """
+
+    # build tree
+    files_in_root = sorted(["file_in_root_dir.txt", "another_file_in_root_dir.md"])
+    files_in_first_folder = sorted(
+        ["first_file.py", "second_file.css", "empty_directory"]
+    )
+    files_in_second_folder = sorted(
+        ["third_file.py", "fourth_file.py", "fifth_file.readme"]
+    )
+
+    tree = Tree(Branch("root"))
+    for f in files_in_root:
+        tree.add_leaf(f)
+
+    first_folder = Branch("first_folder")
+    for f in files_in_first_folder:
+        first_folder.add_leaf(f)
+
+    second_folder = Branch("second_folder")
+    for f in files_in_second_folder:
+        second_folder.add_leaf(f)
+
+    third_folder = Branch("third_folder")
+
+    first_folder.add_child_branch(second_folder)
+    first_folder.add_child_branch(third_folder)
+    tree.add_branch(first_folder)
+
+    # get paths
+    expected_items = [Path(tree.root.name) / fn for fn in files_in_root]
+    expected_items.extend(
+        [Path(tree.root.name) / "first_folder" / fn for fn in files_in_first_folder]
+    )
+    expected_items.extend(
+        [
+            Path(tree.root.name) / "first_folder" / "second_folder" / fn
+            for fn in files_in_second_folder
+        ]
+    )
+    expected_items.extend([Path(tree.root.name) / "first_folder" / "third_folder"])
+
+    expected_paths = [str(p) for p in expected_items]
+    assert set(tree.paths) == set(expected_paths)
