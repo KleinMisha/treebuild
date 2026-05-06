9214a0d27d1e645606066a2f9556fa0bd27ce1e3
KleinMisha <klein.misha@gmail.com>
2026-05-06 11:11:08 +0200

    Custom exception handler (use as decorator).

--- src/treebuild/cli/helpers.py
+++ src/treebuild/cli/helpers.py
@@ -1,8 +1,11 @@
 """Functions shared across CLI endpoints"""
 
+import logging
 from pathlib import Path
 
 from typer import Exit, echo
+
+from treebuild.core.exceptions import TreeBuildError
 
 # Where messages are stored.
 MESSAGES_DIR = Path(__file__).parent / "messages"
@@ -13,9 +16,23 @@ def load_message(filename: str) -> str:
     return file.read_text()
 
 
+# TODO remove this method after refactor / migration is done!
 def ensure_session_exists(session_file: Path) -> None:
     """Most commands need share this check: Ensure a session file has been created."""
     if not session_file.exists():
         msg = load_message("status_no_tree.md")
         echo(msg)
         raise Exit(code=1)
+
+
+# Exception Handlers:
+def raise_exit(exc: TreeBuildError) -> None:
+    """Handle fatal errors by logging message + exiting with status code 1."""
+    logging.error(f"{type(exc).__name__}: {str(exc)}")
+    raise Exit(code=1)
+
+
+def only_warn(exc: TreeBuildError) -> None:
+    """Log the error, do not raise Exit status"""
+    msg = f"{type(exc).__name__} : {str(exc)}"
+    logging.warning(msg)
--- src/treebuild/core/exceptions.py
+++ src/treebuild/core/exceptions.py
@@ -1,5 +1,8 @@
 """Custom exception hierarchy"""
 
+import functools
+from typing import Any, Callable
+
 
 class TreeBuildError(Exception):
     """Base for all treebuild related errors."""
@@ -11,4 +14,41 @@ class DuplicatePathError(TreeBuildError): ...
 class EmptySessionError(TreeBuildError): ...
 
 
+class SessionAlreadyExistsError(TreeBuildError): ...
+
+
 class TreeAssemblyError(TreeBuildError): ...
+
+
+CliCommand = Callable[..., None]
+ErrorHandler = Callable[[TreeBuildError], None]
+
+
+def with_error_handling(
+    handlers: dict[type[TreeBuildError], ErrorHandler],
+) -> Callable[[CliCommand], CliCommand]:
+    """
+    Handle exceptions using a custom handler function for a given exception type.
+    -----
+
+    Supply a mapping from exception (types) --> handler function
+    NOTE if some other exception occurs, other than the ones explicitly defined in the mapping,
+        intentionally letting the programme crash as this indicates a programming error.
+    """
+
+    def decorator(command: CliCommand) -> CliCommand:
+        @functools.wraps(command)
+        def wrapper(*args: Any, **kwargs: Any) -> None:
+            try:
+                command(*args, **kwargs)
+            except TreeBuildError as e:
+                try:
+                    handlers[type(e)](e)
+                except KeyError:
+                    raise KeyError(
+                        f"No handler defined for exception {type(e).__name__} on method {command.__name__}"
+                    )
+
+        return wrapper
+
+    return decorator
