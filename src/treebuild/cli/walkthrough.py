f8a74c3fb83983a1d8fcef188eb347623cd8ed8f
KleinMisha <klein.misha@gmail.com>
2026-05-08 11:56:31 +0200

    test can use constant of expected commands.

--- src/treebuild/cli/walkthrough.py
+++ src/treebuild/cli/walkthrough.py
@@ -24,12 +24,59 @@ from treebuild.core.exceptions import (
 # A CLI command (implementation) is a function with a zero return.
 CliCommand = Callable[[], None]
 
+# Define the expected inputs for the interactive demo. Makes it adjustable / testable (tests can input the 'public' constant)
+
+_PAUSE = "PAUSE"  # sentinel value for typer.pause() input
+_PLANT_CMD = "treebuild plant"
+_SEED_CMD = "treebuild seed demo-project"
+_STATUS_CMD = "treebuild status"
+_ADD_FILE_CMD = "treebuild grow main.py"
+_ADD_SAME_FILE_CMD = "treebuild grow demo-project/main.py"
+_ADD_EMPTY_DIR_CMD = "treebuild grow empty-dummy-dir/"
+_DEMO_PATHS = [
+    "README.md",
+    ".dotfile",
+    "settings.yml",
+    "src/orders.py",
+    "src/users.py",
+    "src/scripts/cleanup.sh",
+    "tests/",
+]
+_GROW_MULTIPLE_CMD = f"treebuild grow {' '.join(_DEMO_PATHS)}"
+_REMOVE_EMPTY_DIR_CMD = "treebuild prune empty-dummy-dir/"
+_RENDER_TO_TEXT_CMD = "treebuild harvest text"
+_MATERIALIZE_TO_FILESYSTEM_CMD = "treebuild harvest scaffold --gitkeep"
+_DEMATERIALIZE_FROM_FILESYSTEM_CMD = "treebuild harvest teardown"
+_CHOP_CMD = "treebuild chop"
+CORRECT_DEMO_INPUTS = [
+    _PAUSE,  # step 1 (press any key to start)
+    _PLANT_CMD,  # step 2
+    _SEED_CMD,  # step 3
+    _STATUS_CMD,  # step 4
+    _ADD_FILE_CMD,  # step 5
+    _ADD_SAME_FILE_CMD,  # step 6
+    _PAUSE,  # pause for info on adding duplicates
+    _ADD_EMPTY_DIR_CMD,  # step 7
+    _STATUS_CMD,  # step 8,
+    _GROW_MULTIPLE_CMD,  # step 9
+    _PAUSE,  # pause
+    _STATUS_CMD,  # step 10
+    _REMOVE_EMPTY_DIR_CMD,  # step 11
+    _STATUS_CMD,  # step 12
+    _RENDER_TO_TEXT_CMD,  # step 13
+    _MATERIALIZE_TO_FILESYSTEM_CMD,  # step 14
+    _PAUSE,  # pause after creation
+    _PAUSE,  # pause before entering cleanup (step 15)
+    _DEMATERIALIZE_FROM_FILESYSTEM_CMD,  # step 15 a
+    _CHOP_CMD,  # step 15 b
+]
+
 
 def prompt_command(correct_command: str, on_success: CliCommand) -> None:
     """Prompt user to type the required command to proceed with the demo."""
     while True:
         user_input = prompt(">")
-        if user_input == correct_command:
+        if user_input.strip() == correct_command:
             on_success()
             break
         else:
@@ -56,139 +103,127 @@ def interactive_demo() -> None:
     # 2. Plant a new tree
     echo("Let's plant a new tree!")
     echo("\n" + "-" * 40)
-    echo(
-        "Use `treebuild plant` to create a new file to store you're tree's paths into."
-    )
-    prompt_command(correct_command="treebuild plant", on_success=plant_impl)
+    echo(f"Use `{_PLANT_CMD}` to create a new file to store you're tree's paths into.")
+    prompt_command(correct_command=_PLANT_CMD, on_success=plant_impl)
 
     # 3. Set name of root directory / give it a name
     echo(
-        "Now let's  name of the root directory 'demo-project' by calling `treebuild seed demo-project`"
+        f"Now let's  name of the root directory 'demo-project' by calling `{_SEED_CMD}`"
     )
     prompt_command(
-        correct_command="treebuild seed demo-project",
+        correct_command=_SEED_CMD,
         on_success=partial(seed_impl, root_name="demo-project", force=False),
     )
 
     # 4. Check your progress
-    echo("You can check the status of your tree using the `treebuild status` command.")
-    prompt_command(correct_command="treebuild status", on_success=status_impl)
+    echo(f"You can check the status of your tree using the `{_STATUS_CMD}` command.")
+    prompt_command(correct_command=_STATUS_CMD, on_success=status_impl)
 
     # 5. Add a file
     echo(
-        "We can now use the `grow` and `prune` commands to add/remove files and directories to this project's tree."
-    )
-    echo(
-        "Add a file called `main.py` in the root directory by calling `treebuild grow main.py`"
+        "We can now use the `grow` and `prune` commands to add/remove files and directories to this project's tree.\n "
+        f"Add a file called `main.py` in the root directory by calling `{_ADD_FILE_CMD}`"
     )
     prompt_command(
-        correct_command="treebuild grow main.py",
+        correct_command=_ADD_FILE_CMD,
         on_success=partial(grow_impl, paths=["main.py"]),
     )
 
     # 6. Try adding the same file, but include root-dir as prefix --> Duplicates are skipped automatically
     echo(
-        "Let's see what happens if you add the same file again, this time explicitly stating the root as prefix"
+        "Let's see what happens if you add the same file again, this time explicitly stating the root as prefix\n"
+        f"Use `{_ADD_SAME_FILE_CMD}`"
     )
-    echo("Use `treebuild grow demo-project/main.py`")
     prompt_command(
-        correct_command="treebuild grow demo-project/main.py",
+        correct_command=_ADD_SAME_FILE_CMD,
         on_success=partial(grow_impl, paths=["demo-project/main.py"]),
     )
-    echo(
+    pause(
         "Would you look at that! Treebuild understands you've already added this file and will simply skip it, so you don't need to worry about accidentally adding the same file twice."
     )
 
     # 7. Add an (empty) directory (NOTE the trailing slash)
     echo(
-        "Add an empty directory using `treebuild grow empty-dummy-dir/`."
+        f"Add an empty directory using `{_ADD_EMPTY_DIR_CMD}`."
         "Note the trailing slash in the entered path (signaling a directory)."
     )
     prompt_command(
-        correct_command="treebuild grow empty-dummy-dir/",
+        correct_command=_ADD_EMPTY_DIR_CMD,
         on_success=partial(grow_impl, paths=["empty-dummy-dir/"]),
     )
 
     # 8. Check your progress
-    echo("Time to double-check our progress: `treebuild status`")
-    prompt_command(correct_command="treebuild status", on_success=status_impl)
+    echo(f"Time to double-check our progress: {_STATUS_CMD}")
+    prompt_command(correct_command=_STATUS_CMD, on_success=status_impl)
 
     # 9. Add a bunch of additional files / directories (NOTE if parent dir is not empty, you can simple add the full path in one go)
-    paths_to_add = [
-        "README.md",
-        ".dotfile",
-        "settings.yml",
-        "src/orders.py",
-        "src/users.py",
-        "src/scripts/cleanup.sh",
-        "tests/",
-    ]
+
     echo(
         "We can actually add multiple items in a single call, let's do such to make this project a bit more interesting. "
     )
-    echo(f"Call `treebuild grow {paths_to_add}`")
+    echo(f"Call `{_GROW_MULTIPLE_CMD}`")
     prompt_command(
-        correct_command=f"treebuild grow {paths_to_add}",
-        on_success=partial(grow_impl, paths=paths_to_add),
+        correct_command=_GROW_MULTIPLE_CMD,
+        on_success=partial(grow_impl, paths=_DEMO_PATHS),
     )
-    echo("All paths are interpreted as being relative to the root's directory. ")
-    echo(
+    pause(
+        "All paths are interpreted as being relative to the root's directory."
         "Also note how files placed in nested directories can be added directly by supplying the full path."
     )
 
     # 10. Check your progress
-    echo("Check your progress: `treebuild status`")
-    prompt_command(correct_command="treebuild status", on_success=status_impl)
+    echo(f"Check your progress: `{_STATUS_CMD}`")
+    prompt_command(correct_command=_STATUS_CMD, on_success=status_impl)
 
     # 11. Remove a path we actually do not want
     echo(
-        "Oops! We still seem to have the dummy directory, remove it by calling `treebuild prune empty-dummy-dir/`"
+        f"Oops! We still seem to have the dummy directory, remove it by calling `{_REMOVE_EMPTY_DIR_CMD}`"
     )
     prompt_command(
-        correct_command="treebuild prune empty-dummy-dir/",
+        correct_command=_REMOVE_EMPTY_DIR_CMD,
         on_success=partial(prune_impl, paths=["empty-dummy-dir/"]),
     )
 
     # 12. Check your progress
-    echo("Check your progress: `treebuild status`")
-    prompt_command(correct_command="treebuild status", on_success=status_impl)
+    echo(f"Check your progress: `{_STATUS_CMD}`")
+    prompt_command(correct_command=_STATUS_CMD, on_success=status_impl)
 
     # 13. Render your tree (using default renderer)
     echo("This looks good. We can now 'harvest' what we've grown. ")
     echo(
-        "Let's start by rendering a tree structure by calling `treebuild harvest text`"
+        f"Let's start by rendering a tree structure by calling `{_RENDER_TO_TEXT_CMD}`"
     )
-    prompt_command(correct_command="treebuild harvest text", on_success=show_rendering)
-    echo(
+    prompt_command(correct_command=_RENDER_TO_TEXT_CMD, on_success=show_rendering)
+    pause(
         "We used a vanilla plain text rendering here, "
         "You're encouraged to checkout `treebuild harvest text --help` to checkout the other options for the `--rendering` flag. "
     )
     # 13. Materialize the tree
     echo(
-        "That looks good! Let's immediately materialize this project to the filesystem by calling `treebuild harvest scaffold --gitkeep`"
+        f"That looks good! Let's immediately materialize this project to the filesystem by calling `{_MATERIALIZE_TO_FILESYSTEM_CMD}`"
     )
     prompt_command(
-        correct_command="treebuild harvest scaffold --gitkeep",
+        correct_command=_MATERIALIZE_TO_FILESYSTEM_CMD,
         on_success=partial(scaffold_impl, gitkeep=True),
     )
-    echo(
+    pause(
         "The `--gitkeep` flag has added a `.gitkeep` file into any empty directory detected, such that it can be committed to a (remote) repository."
     )
-
-    # 14. You can inspect the tree at "show path", when ready .. we will remove the created files
     pause(
         "Ain't that cool? The project has been al setup. "
         "Check it out, and press any key to proceed into the cleanup procedure (will reset your state to before the demo.)"
     )
 
-    # 15. Cleanup
-    echo("Remove the files and directories created: `treebuild harvest teardown`")
+    # 14. Cleanup
+    echo(
+        f"Remove the files and directories created: `{_DEMATERIALIZE_FROM_FILESYSTEM_CMD}`"
+    )
     prompt_command(
-        correct_command="treebuild harvest teardown", on_success=teardown_impl
+        correct_command=_DEMATERIALIZE_FROM_FILESYSTEM_CMD, on_success=teardown_impl
     )
 
-    echo("Remove the file storing your paths: `treebuild chop`")
-    prompt_command(correct_command="treebuild chop", on_success=chop_impl)
+    echo(f"Remove the file storing your paths: `{_CHOP_CMD}`")
+    prompt_command(correct_command=_CHOP_CMD, on_success=chop_impl)
 
     # 15. Done with the demo ---> Enter quickstart guide?
     enter_quickstart = confirm(
