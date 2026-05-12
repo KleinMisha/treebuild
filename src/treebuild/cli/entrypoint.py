4d7036e6f64e4046db6dad1e79e7ea471d4dc9be
KleinMisha <klein.misha@gmail.com>
2026-05-13 01:01:18 +0200

    Invoking treebuild without any command shows the help message. Enforced with test.

--- src/treebuild/cli/entrypoint.py
+++ src/treebuild/cli/entrypoint.py
@@ -2,7 +2,7 @@
 
 from typing import Annotated
 
-from typer import Option, Typer
+from typer import Context, Option, Typer, echo
 
 from treebuild.cli.routing.harvest import harvest_app
 from treebuild.cli.routing.treebuild import app as main_app
@@ -14,8 +14,9 @@ app.add_typer(main_app)
 app.add_typer(harvest_app, name="harvest")
 
 
-@app.callback()
+@app.callback(invoke_without_command=True)
 def main(
+    ctx: Context,
     verbose: Annotated[
         bool, Option("--verbose", "-v", help="Show detailed output.")
     ] = False,
@@ -23,6 +24,9 @@ def main(
         bool, Option("--quiet", "-q", help="Suppress all messages except errors.")
     ] = False,
 ) -> None:
+    # if tool name (treebuild) is typed without any command --> show help message
+    if ctx.invoked_subcommand is None:
+        echo(ctx.get_help())
 
     # setup logging with logging level based on flags
     setup_logging(verbose=verbose, quiet=quiet)
