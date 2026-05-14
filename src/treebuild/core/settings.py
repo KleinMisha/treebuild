c0ba21300a02e6daaaf0ebcf04d3a6bf91e93197
KleinMisha <klein.misha@gmail.com>
2026-05-14 14:59:19 +0200

    Adjusted priority of settings. ENV (only for testing purposes) > Local settings file > Global settings file > Pydantic model initializer.

--- src/treebuild/core/settings.py
+++ src/treebuild/core/settings.py
@@ -1,6 +1,7 @@
 """Let Pydantic-settings take care of loading settings from ~/.config/treebuild/config.toml"""
 
 from pathlib import Path
+from typing import ClassVar
 
 from pydantic_settings import (
     BaseSettings,
@@ -12,8 +13,11 @@ from pydantic_settings import (
 from treebuild.harvest.render_factory import RenderMethod
 
 # DEFAULTS TO FALL BACK TO:
-DEFAULT_SESSION_FILE = Path().home() / ".config" / "treebuild" / "session_tree.txt"
 ENV_PREFIX = "TREEBUILD_"
+GLOBAL_TREEBUILD_DIR = Path().home() / ".config" / "treebuild"
+LOCAL_TREEBUILD_DIR = Path(".treebuild/")
+DEFAULT_SESSION_FILE = "session_tree.txt"
+DEFAULT_SETTINGS_FILE = "settings.toml"
 
 
 class TreeBuildSettings(BaseSettings):
@@ -23,7 +27,7 @@ class TreeBuildSettings(BaseSettings):
         toml_file=Path().home() / ".config" / "treebuild" / "config.toml",
         env_prefix=ENV_PREFIX,
     )
-    session_file: Path = DEFAULT_SESSION_FILE
+    session_file: Path = GLOBAL_TREEBUILD_DIR / DEFAULT_SESSION_FILE
     renderer: RenderMethod = RenderMethod.PLAIN
 
     @classmethod
@@ -39,18 +43,24 @@ class TreeBuildSettings(BaseSettings):
         Priority of loading variables
 
         1. Constructor (TreeBuildSettings(key=value), aka the __init__() method)
-        2. Environment variables (so you can 'env export variables' in terminal or `.zshrc`-like file)
-        3. config TOML file (~/.config/treebuild/config.toml)
+        2. Global config TOML file (~/.config/treebuild/settings.toml)
+        3. Local config TOML file (<root>/.config/treebuild/settings.toml)
+        4. Environment variables (so you can 'env export variables' in terminal or `.zshrc`-like file)
 
         ---
-        NOTE: Setting environment variables is optional, hence excluding some manual overwrite during instantiation,
-        values will be parsed from the TOML file.
+        NOTE: ENV variables will only be used for testing purposes. Not intended to be utilized by the user (if they do, it will overwrite any local settings)
         """
 
+        LOCAL_SETTINGS_PATH = LOCAL_TREEBUILD_DIR / DEFAULT_SETTINGS_FILE
+        GLOBAL_SETTINGS_PATH: Path = GLOBAL_TREEBUILD_DIR / DEFAULT_SETTINGS_FILE
         return (
             init_settings,
+            TomlConfigSettingsSource(settings_cls, toml_file=GLOBAL_SETTINGS_PATH),
+            TomlConfigSettingsSource(
+                settings_cls,
+                toml_file=LOCAL_SETTINGS_PATH if LOCAL_SETTINGS_PATH else None,
+            ),
             env_settings,
-            TomlConfigSettingsSource(settings_cls),
         )
 
 
